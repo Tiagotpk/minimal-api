905f75b399b2d4e62d913922b92c7a661ed8056e
Tiago Reis <ttrpk@hotmail.com>
2024-09-04 15:03:57 -0300

    alteracoes

--- Test/obj/Debug/net8.0/test.AssemblyInfo.cs
+++ Test/obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+017b0eb6878cbaa2aa86aabbbbf9271498416a3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32ba0a774697ae8209ca1be9c38aae8df4e7e77f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
